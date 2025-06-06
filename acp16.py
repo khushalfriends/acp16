d9c18da7143a2a597bef5b12b70b50e31aa80a74
khushalfriends <khusalagarwalla@gmail.com>
2025-06-06 18:07:05 +0530

    Add files via upload

--- acp16.py
+++ acp16.py
@@ -1,1 +1,7 @@
-def due_amount(,)
+def due_amount(total_bill_amount, paid_amount):
+    due_amount = total_bill_amount
+    return due_bill
+
+total_bill_amount = float(input("Enter the total bill amount: "))
+paid_amount = float(input("Enter the amount paid already: "))
+print("Due amount: ₹", due_amount(total_bill_amount, paid_amount))
